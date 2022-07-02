b8ea4fe11deb3854d6037916fc364ed80062c2cc
ayushkhandare <108357023+ayushkhandare@users.noreply.github.com>
2022-07-02 18:06:47 +0530

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,51 +1,79 @@
-from cProfile import label
-import imp
-from lib2to3.pgen2.pgen import generate_grammar
-from msilib.schema import CheckBox
 from tkinter import *
 from PIL import Image, ImageTk
+from tkinter import messagebox
 
-###main window
-root = Tk()
-root.geometry('1200x750')
-root.title("PASSWORD GENERATOR")
-root.minsize(550, 550)
 
-###Bg Image
-'''photo = Image.open('Password generator/4.jpg')
-picture = ImageTk.PhotoImage(photo)
-pict = Label(image = picture)
-pict.place(x = 0, y = 0, relwidth=1, relheight=1)'''
+def parameters_UI():
+    ###main window
+    root = Tk()
+    root.geometry('250x250')
+    root.title("PASSWORD GENERATOR")
+    root.minsize(550, 550)
+    root.configure(bg = 'white')
 
-###Main Heading
-heading = Label(text = 'Welcome to Password Generator', font = 'comicsansms 25 bold', bg = 'black', fg = 'red')
-heading.pack()
 
-###Frame for criterias
-frame = LabelFrame(root, padx = 20, pady = 20, bg = '#ADD8E6', relief = RAISED, borderwidth=7)
-frame.pack(anchor = 'nw', padx = 30, pady = 100)
+    ###Bg Image
+    '''photo = Image.open('Password generator/4.jpg')
+    picture = ImageTk.PhotoImage(photo)
+    pict = Label(image = picture)
+    pict.place(x = 0, y = 0, relwidth=1, relheight=1)'''
 
-###Heading for criterias
-criteria = Label(frame, text = '\n\nSelect the criterias for your password', font = 'helvetica 15 bold', fg = 'red', bg = '#ADD8E6')
-criteria.pack(anchor= 'nw')
 
-# Special characters in password(Y/N): y
-# Numbers in password(Y/N): y
-# Enter length of password: 25
+    ###Frame for criterias
+    frame = LabelFrame(root, padx = 20, pady = 20, bg = '#ADD8E6', relief = RAISED, borderwidth=7)
+    frame.place(x = 30, y = 100)
 
-special_char = Checkbutton(frame, text = 'Special Characters', font = 'helvetica 10 bold', bg = '#ADD8E6')
-special_char.pack(anchor = 'nw', pady = 10)
+    ###Heading for criterias
+    criteria = Label(frame, text = '\n\nSelect the criterias for your password', font = 'helvetica 15 bold', fg = 'black', bg = '#ADD8E6')
+    criteria.pack(anchor= 'nw')
 
-digits = Checkbutton(frame, text = 'Numbers in password', font = 'helvetica 10 bold', bg = '#ADD8E6')
-digits.pack(anchor = 'nw')
+    ###Checkbox Command
+    def checkbox():
+        response_special.config(text = var.get())
+        response_digits.config(text = var1.get())
+        global inp 
+        inp = len_entry.get(1.0, "end-1c")
+        root.destroy()
+        
 
-length = LabelFrame(frame, text = 'Enter the length of password you want to generate', bg = '#ADD8E6', font = 'helvetica 10 bold')
-length.pack(anchor = 'nw', pady = 15, padx = 10)
+    ###Checkbox variable
+    var = StringVar(value = 'No Characters')
+    var1 = StringVar(value = 'No digits')
 
-len_entry = Entry(length, font = 'helvetica 20')
-len_entry.pack(pady = 20, padx = 10)
+    ###Creating Checkboxes
+    special_char = Checkbutton(frame, text = 'Special Characters', variable=var, onvalue="Include Char",offvalue='No Characters', font = 'helvetica 10 bold', bg = '#ADD8E6')
+    special_char.pack(anchor = 'nw', pady = 10)
 
-generate = Button(frame, text = 'Generate Password', font = 'comicsansms 13')
-generate.pack()
+    digits = Checkbutton(frame, text = 'Numbers in password', onvalue="Include digits", offvalue="No digits", variable=var1, font = 'helvetica 10 bold', bg = '#ADD8E6')
+    digits.pack(anchor = 'nw')
 
-root.mainloop()
+    response_special = Label(root)
+    #response_special.place(x = 30, y = 450)
+
+    response_digits = Label(root)
+    #response_digits.place(x = 30, y = 480)
+
+    #Creating Length Criteria
+    length = LabelFrame(frame, text = 'Enter the length of password you want to generate', bg = '#ADD8E6', font = 'helvetica 12 bold')
+    length.pack(anchor = 'nw', pady = 15, padx = 10)
+    len_entry = Text(length, height = 1, width = 20)
+    len_entry.pack(pady = 20, padx = 10)
+
+
+    ###Creating Generate Button
+    generate = Button(frame, text = 'Generate Password', font = 'comicsansms 13', command = checkbox)
+    generate.pack()
+    root.mainloop()
+    
+    if var.get() == 'No Characters':
+        spchar = False
+    else:
+        spchar = True
+    if var1.get() == 'No digits':
+        num = False
+    else:
+        num = True
+    return spchar, num, inp
+
+def invalid_UI():
+    messagebox.showerror("showerror", "Invalid Input!")
