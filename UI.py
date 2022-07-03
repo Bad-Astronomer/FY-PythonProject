ee7e4048e6fd3764796db5f9dd9b429617d1d40e
ShivomKarnad <108384118+ShivomKarnad@users.noreply.github.com>
2022-07-03 16:46:27 +0530

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from PIL import Image, ImageTk
-from tkinter import messagebox
 
 
 def parameters_UI():
@@ -176,3 +175,45 @@ def display_UI():
 
     root.mainloop()
     return a
+
+
+def pwd_display_UI(caption, password):
+    tags = caption
+    passwords = password
+
+    window = Tk()
+    window.geometry('750x600')
+    window.title("PASSWORD GENERATOR")
+    window.minsize(550, 550)
+    window.configure(bg='white')
+
+    def ok_button():
+        window.destroy()
+
+    title = Label(window, text="SAVED PASSWORDS", bg = "white",
+                font="helvetica 20 bold")
+    title.pack(pady=20)
+
+    frame = LabelFrame(window, padx=20, pady=20, bg='#ADD8E6',
+                    relief=RAISED, borderwidth=7)
+    frame.pack(pady=50, padx=50)
+
+    scrollbar = Scrollbar(frame)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    disp = Listbox(frame, bg='#ADD8E6', font='helvetica 17',
+                yscrollcommand=scrollbar.set)
+
+    # before execution check if len(passwords) == len(tags) else call Invalid_UI
+    for i in range(len(passwords)):
+        disp.insert(END, tags[i] + ':')
+        disp.itemconfig(END, bg='#3aeb34')
+        disp.insert(END, passwords[i])
+        disp.itemconfig(END, bg='#eb9334')
+        disp.insert(END, "")
+        disp.pack()
+
+    ok_button = Button(frame, text = 'OK', font = 'helvetica 10', command = ok_button)
+    ok_button.pack(pady = 10, padx=20)
+
+    window.mainloop()
