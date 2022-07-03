8fd91de577626da8c8cb7aaf6be3e2dff61de37e
ShivomKarnad <108384118+ShivomKarnad@users.noreply.github.com>
2022-07-03 16:23:09 +0530

    Update pass_display.py

--- pass_display.py
+++ pass_display.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 tags = ['google', 'github', 'twitch', 'asfja']
 passwords = ['ajsjf', 'rkrss', 'opsde', 'ajdjd']
 
@@ -8,19 +9,33 @@ window.title("PASSWORD GENERATOR")
 window.minsize(550, 550)
 window.configure(bg='white')
 
-title = Label(window, text="SAVED PASSWORDS",
+def ok_button():
+    window.destroy()
+
+title = Label(window, text="SAVED PASSWORDS", bg = "white",
               font="helvetica 20 bold")
 title.pack(pady=20)
 
 frame = LabelFrame(window, padx=20, pady=20, bg='#ADD8E6',
                    relief=RAISED, borderwidth=7)
-frame.pack(pady=50, anchor='nw', padx=50)
+frame.pack(pady=50, padx=50)
 
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+disp = Listbox(frame, bg='#ADD8E6', font='helvetica 17',
+               yscrollcommand=scrollbar.set)
 
 # before execution check if len(passwords) == len(tags) else call Invalid_UI
 for i in range(len(passwords)):
-    disp = Label(frame, text=tags[i] + ": " +
-                 passwords[i], bg='#ADD8E6', font='helvatica 12')
-    disp.pack(anchor="nw")
+    disp.insert(END, tags[i] + ':')
+    disp.itemconfig(END, bg='#3aeb34')
+    disp.insert(END, passwords[i])
+    disp.itemconfig(END, bg='#eb9334')
+    disp.insert(END, "")
+    disp.pack()
+
+ok_button = Button(frame, text = 'OK', font = 'helvetica 10', command = ok_button)
+ok_button.pack(pady = 10, padx=20)
 
 window.mainloop()
